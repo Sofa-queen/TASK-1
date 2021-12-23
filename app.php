61af267603f83673291cbd8e65a244e353d74c51
 iMac <imac@iMac-iMac.local>
2021-12-23 14:08:02 +0300

    try-catch/Exception

--- app.php
+++ app.php
@@ -1,4 +1,10 @@
 <?php
+
+class EmptyException extends Exception { }
+class NameException extends Exception { }
+class ObjException extends Exception { }
+class TypeException extends Exception { }
+
 interface File_Contents
 {
     public function readFile();
@@ -13,6 +19,12 @@ abstract class File implements File_Contents
 
     public function readFile()
     {
+        if(file_exists($this->name) == false)
+            throw new Exception($this->name);
+
+        if(filesize($this->name) == 0)
+            throw new EmptyException($this->name);
+
         echo "\n+==============================+\n";
         echo "+ " . mb_strtoupper($this->type) . " file $this->name +\n";
         echo "+==============================+\n\n";
@@ -52,21 +64,41 @@ class PHP_File extends File
     }
 }
 
-//var_dump($argv);
 class ReadFileFactory
 {
     public static function createFileContent($type, $name)
     {
-        switch ($type) {
-            case 'css':
-                return new CSS_File($type, $name);
-            case 'txt':
-                return new TXT_File($type, $name);
-            case 'php':
-                return new PHP_File($type, $name);
+        try {
+            if($type !== 'css' && $type !== 'txt' && $type !== 'php')
+                throw new TypeException($type);
+
+            if($name == NULL)
+                throw new NameException('ERROR: ');
+
+            switch ($type) {
+                case 'css':
+                    return new CSS_File($type, $name);
+                case 'txt':
+                    return new TXT_File($type, $name);
+                case 'php':
+                    return new PHP_File($type, $name);
+            }
+        } catch (NameException $e) {
+            echo $e->getMessage() . "You didn't specify a file name!!!\n";
+        } catch (TypeException $e) {
+            echo "ERROR: You cannot output a file of the type: " . $e->getMessage() . "\n";
         }
     }
 }
 
 $obj = ReadFileFactory::createFileContent($argv[1], $argv[2]);
-$obj->readFile();
+try {
+    if($obj == NULL) throw new ObjException('ERROR');
+    $obj->readFile();
+} catch (ObjException $e) {
+    echo $e->getMessage() . "!!!\n";
+}catch (EmptyException $e) {
+    echo "The file " . $e->getMessage() . " is empty!!!\n";
+} catch (Exception $e) {
+    echo "The file " . $e->getMessage() . " does not exist!!!\n";
+}
